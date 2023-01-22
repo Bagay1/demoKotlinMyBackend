82214364337b6a6435e7b29f6e4534e6cc2c81d3
Bagay1 <bagaycar1@gamil.com>
2023-01-22 22:13:44 +0500

    Добавлял все по инструкции... не работает

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
+    kotlin("plugin.allopen") version "1.8.0"
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.Embeddable")
+    annotation("jakarta.persistence.MappedSuperclass")
 }
 
 
@@ -24,6 +31,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.testng:testng:7.1.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/kotlin/com/example/demo/Entites.kt
@@ -0,0 +1,30 @@
+package com.example.demo
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.ManyToOne
+import org.springframework.data.annotation.Id
+import java.time.LocalDateTime
+
+//class Entites {
+//
+//
+//}
+
+@Entity
+class Article(
+    var title: String,
+    var headline: String,
+    var content: String,
+    @ManyToOne var author: User,
+    var slug: String = title.toSlug(),
+    var addedAt: LocalDateTime = LocalDateTime.now(),
+    @Id @GeneratedValue var id: Long? = null)
+
+@Entity
+class User(
+    var login: String,
+    var firstname: String,
+    var lastname: String,
+    var description: String? = null,
+    @Id @GeneratedValue var id: Long? = null)
